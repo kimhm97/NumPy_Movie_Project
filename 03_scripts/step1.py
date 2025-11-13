3ab4d5da776b8c18e413f9d57622bcd826a0244a
김혜민 <vbcn123@naver.com>
2025-11-14 00:55:40 +0900

    Initial commit - Step2 setup

--- 03_scripts/step1.py
+++ 03_scripts/step1.py
@@ -26,13 +26,13 @@ def parse_csv_line(line):
 with open(csv_path,"r", encoding="utf-8") as f:
     lines = f.read().splitlines()  # 줄 단위 리스트 생성
 
-# 헤더와 데이터 분리
+# 헤더와 데이터 분리 후
 header = parse_csv_line(lines[0]) # 파싱 헤더 추출
 data_lines = lines[1:]            # 데이터 줄만 따로 저장
 print(header)
 
 # 영화 제목, 장르, 평점 컬럼 인덱스 반환
-title_idx = header.index("Title")  # 1
+title_idx = header.index("Title")   # 1
 genre_idx = header.index("Genre")  # 4
 rate_idx = header.index("Rate")    # 5
 
@@ -45,7 +45,7 @@ for line in data_lines:
 
     # row 길이가 필요한 컬럼 중 가장 큰 인덱스보다 작으면 해당 컬럼이 없다는 의미
     if len(row) <= max(title_idx, genre_idx, rate_idx):
-        continue  # 이 줄은 건너 뛰고, 다음 줄을 처리하러 for문 처음으로 돌아감.
+        continue  # 없다면, 건너 뛴다. 다음 줄을 처리하러 for문 처음으로 돌아감.
 
     # 컬럼 값 가져오기 및 공백 제거
     title = row[title_idx].strip()
@@ -58,10 +58,12 @@ for line in data_lines:
 
     # Title에서 Released_Year 추출
     # Title 마지막에 () 형태가 있어야만 슬라이싱
+    released_year = None
     if '(' in title and title[-1] == ')':
         released_year = title[title.rfind('(')+1 : title.rfind(')')]
     else:
-        continue #연도가 없으면 건너뛰기
+        # 괄호가 없으면 임의로 0 처리
+        released_year = 0
 
     # Rate와 Released_Year 타입 변환 후 리스트에 추가
     try:
@@ -73,6 +75,7 @@ for line in data_lines:
 # NumPy 배열로 변환
 # dtype=object 사용 : 문자열 + 숫자가 혼합된 배열
 data_array = np.array(processed_data, dtype=object)
+print(data_array)
 
 # 결과 확인
 print("NumPy 배열 형태의 데이터 출력: ", data_array)
---
+++ 03_scripts/step2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# NumPy 배열 불러오기
+data_array = np.load("../04_outputs/data_array.npy", allow_pickle=True)
+print(data_array)
+
+# 총 영화 개수
+total_movies = data_array.shape[0]  # NumPy 배열 행 수 = 영화 개수
+print("총 영화 개수:", total_movies)
+
+# 평점 (Rate) 관련 통계
+# 평점은 배열의 3번째 컬럼 (인덱스 2)
+ratings = data_array[:, 2]  # 평점 컬럼 추출
+ave_rat = np.mean(ratings)  # 평점
+max_rat = np.max(ratings)   # 최고
+min_rat = np.min(ratings)   # 최저
+
+print(f"평균 평점: {ave_rat:.1f}\n최고 평점: {max_rat}\n최저 평점: {min_rat}")
