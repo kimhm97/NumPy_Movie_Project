184cd16f13b55f21e4c5b5fe3503f9d7a129e0ac
김혜민 <vbcn123@naver.com>
2025-11-14 00:59:09 +0900

    Initial commit - Step2 setup

--- 03_scripts/step2.py
+++ 03_scripts/step2.py
@@ -11,7 +11,7 @@ print("총 영화 개수:", total_movies)
 # 평점 (Rate) 관련 통계
 # 평점은 배열의 3번째 컬럼 (인덱스 2)
 ratings = data_array[:, 2]  # 평점 컬럼 추출
-ave_rat = np.mean(ratings)  # 평점
+ave_rat = np.mean(ratings)  # 평균
 max_rat = np.max(ratings)   # 최고
 min_rat = np.min(ratings)   # 최저
 
